e9346fe8b4acff6e720d1af8b838f72ef365f69c
Dishan <dishan@outlook.com>
2020-12-14 00:30:03 -0500

    new changes

--- src/app/core/shared/promo.service.ts
+++ src/app/core/shared/promo.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Injectable } from "@angular/core";
+import { AngularFireDatabase } from "angularfire2/database";
+import { HttpClient } from "@angular/common/http";
 
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
-import { Promo } from '../../models/promo.model';
+import { Promo } from "../../models/promo.model";
+import { filter,map } from "rxjs/operators";
 
 @Injectable()
 export class PromoService {
-  constructor(private angularFireDatabase: AngularFireDatabase) {}
+  jsonUrl = "../../../assets/data/demo_data.json";
+
+  constructor(
+    private angularFireDatabase: AngularFireDatabase,
+    private http: HttpClient
+  ) {}
 
   getPromos(): Observable<Promo[]> {
-    return this.angularFireDatabase.list<Promo>('promos').valueChanges();
+    return this.http.get(this.jsonUrl)
+    .pipe(
+      map(data => (data['promos']))
+    );
+    return this.angularFireDatabase.list<Promo>("promos").valueChanges();
   }
 }
+// export class PromoService {
+//   constructor(private angularFireDatabase: AngularFireDatabase) {}
+
+//   getPromos(): Observable<Promo[]> {
+//     return this.angularFireDatabase.list<Promo>('promos').valueChanges();
+//   }
+// }
